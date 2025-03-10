94cd5ced14f2d05fe26bc1b289258910d1cbdd1d
andrewteece <andrewteece@gmail.com>
2025-03-10 07:19:18 -0500

    removed LinkContainer use Link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,32 +1,44 @@
-import { Navbar, Nav, Container, NavbarToggle } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../assets/logo.png'
 
 const Header = () => {
+  //   const navigate = useNavigate()
+
+  //   // const logoutHandler = async () => {
+  //   //   try {
+  //   //     await logoutApiCall().unwrap()
+  //   //     dispatchEvent(logout())
+  //   //     // NOTE: here we need to reset cart state for when a user logs out so the next
+  //   //     // user doesn't inherit the previous users cart and shipping
+  //   //     dispatchEvent(resetCart())
+  //   //     navigate('/login')
+  //   //   } catch (err) {
+  //   //     console.error(err)
+  //   //   }
+  //   // }
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand>
-              <img src={logo} alt='ProShop' />
-              ProShop
-            </Navbar.Brand>
-          </LinkContainer>
+          <Navbar.Brand as={Link} to='/'>
+            <img src={logo} alt='ProShop' />
+            ProShop
+          </Navbar.Brand>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <LinkContainer to='/cart'>
-                <Nav.Link>
-                  <FaShoppingCart /> Cart
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link href='/login'>
-                  <FaUser /> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              <Nav.Link as={Link} to='/cart'>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+
+              <Nav.Link as={Link} to='/login'>
+                <FaUser /> Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
