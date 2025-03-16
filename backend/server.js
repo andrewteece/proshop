9c5b000c2534ab1f572bfdab50424def3b79cfef
andrewteece <andrewteece@gmail.com>
2025-03-16 15:41:24 -0500

    add body parser

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ dotenv.config()
 
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-//import products from './data/products.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 const port = process.env.PORT || 3000;
@@ -14,12 +13,15 @@ connectDB(); // connect to MongoDB
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send('API is running...');
-// })
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 
 app.use(notFound)
