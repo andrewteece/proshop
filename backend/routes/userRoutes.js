4702403bc3ad53be08a4325e0a262010e114fd73
andrewteece <andrewteece@gmail.com>
2025-03-18 08:11:55 -0500

    authMiddleware and update userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,15 +10,25 @@ import {
     getUserById,
     updateUser
 } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser).get(getUsers);
-router.route('/logout', logoutUser);
-router.route('/login').post(authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
-router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
+
+
 
 export default router;
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 
+import cookieParser from 'cookie-parser';
+
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js'
@@ -16,6 +18,9 @@ const app = express();
 // body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+// cookie parser middleware
+app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
