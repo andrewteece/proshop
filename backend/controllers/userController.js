fc8e30d6525e0d830b6f784135ef07837fcda634
andrewteece <andrewteece@gmail.com>
2025-03-25 10:50:02 -0400

    fixed generateToken and updated userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
-import jwt from 'jsonwebtoken'
+//import jwt from 'jsonwebtoken'
 import asyncHandler from '../middleware/asyncHandler.js'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js';
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -11,17 +12,19 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '1d',
-    });
+    generateToken(res, user._id);
 
-    // Set JWT as an HTTP-Only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-      sameSite: 'strict', // Prevent CSRF attacks
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    });
+    // const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    //   expiresIn: '1d',
+    // });
+
+    // // Set JWT as an HTTP-Only cookie
+    // res.cookie('jwt', token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    //   sameSite: 'strict', // Prevent CSRF attacks
+    //   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    // });
 
     res.json({
       _id: user._id,
@@ -57,6 +60,8 @@ const authUser = asyncHandler(async (req, res) => {
     })
 
     if (user) {
+      generateToken(res, user._id);
+      
       res.status(201).json({
         _id: user._id,
         name: user.name,
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (res, userId) => {
-    const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: '1h',
     });
 
     // set JWT as an HTTP-Only cookie
-    res.cookie('jwt', token {
+    res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'devlopment', // user secure cookies in production
         sameSite: 'strict', // prevent CSRF attacks
