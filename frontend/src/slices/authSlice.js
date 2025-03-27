de16414d145cee7d7a097bfd9d0d01d158bea9f0
andrewteece <andrewteece@gmail.com>
2025-03-27 11:02:34 -0400

    update constants and create authSlice

---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const intialState = {
+    userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null,
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    intialState,
+    reducers: {
+        setCredentials: (state, action) => {
+            state.userInfo = action.payload;
+            localStorage.setItem('userInfo', JSON.stringify(action.payload));
+        },
+    },
+});
+
+export const { setCredentials } = authSlice.actions;
+
+export default authSlice.reducer;
