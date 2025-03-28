ffac9e5d8e284320897c53b6b24b722e5e144c2a
andrewteece <andrewteece@gmail.com>
2025-03-28 10:29:45 -0400

    install toast and refactor LoginScreen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col, NavItem } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
+import Loader from '../components/Loader';
+import { useLoginMutation } from '../slices/usersApiSlice.js';
+import { setCredentials } from '../slices/authSlice.js';
+import { toast } from 'react-toastify';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get('redirect') || '/';
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log('submitHandler');
+    try {
+        const res = await login({ email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate(redirect);
+    } catch (err) {
+        toast.error(err?.data?.message || err.error);
+    }
   };
 
   return (
