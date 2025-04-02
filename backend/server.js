4ea8849cfb209087bfb4ce91939e6d49f9e3771f
andrewteece <andrewteece@gmail.com>
2025-04-02 08:51:53 -0400

    update server and orderController

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js';
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 connectDB(); // connect to MongoDB
 
