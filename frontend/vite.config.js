850a20e56460d3b019d50e5fdb8431ac2ef1992c
andrewteece <andrewteece@gmail.com>
2025-04-05 09:09:55 -0400

    update vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslintPlugin from 'vite-plugin-eslint'
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
